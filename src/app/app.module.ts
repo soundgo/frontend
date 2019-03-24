5e88802cd1a29f4c21a70803734381b49ce860a6
Javier <javiersb1997@gmail.com>
2019-03-24 23:23:04 +0100

    Fix app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,56 +1,46 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MaterialModule} from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
-import {HttpClientModule, HttpClient} from '@angular/common/http';
-import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-import {
-    MapBoxComponent
-} from './sections/map/map-box/map-box.component';
-NumberReproductionsAdvertisementsComponent
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {SharedModule} from './shared/shared.module';
+import { SitePanelSheetComponent } from './sections/map/site-panel-sheet/site-panel-sheet.component';
 
-import {RecordModule} from './sections/record/record.module';
-import {SitePanelSheetComponent} from './sections/map/site-panel-sheet/site-panel-sheet.component';
-
-import {FormsModule} from '@angular/forms';
-import {SharedModule} from './shared/shared.module';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {MapBoxComponent, SitePanelSheet} from './sections/map/map-box/map-box.component';
-import {RecordModule} from './sections/record/record.module';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MapBoxComponent } from './sections/map/map-box/map-box.component';
+import { RecordModule } from './sections/record/record.module';
 
 export function HttpLoaderFactory(http: HttpClient) {
-    return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http);
 }
 
 @NgModule({
-    declarations: [AppComponent, MapBoxComponent, SitePanelSheetComponent],
-    entryComponents: [SitePanelSheetComponent], // inside we put the modalComponent
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        SharedModule,
-        HttpClientModule,
-        FormsModule,
-        BrowserAnimationsModule,
-        MaterialModule,
-        RecordModule,
-        TranslateModule.forRoot({
-            loader: {
-                provide: TranslateLoader,
-                useFactory: HttpLoaderFactory,
-                deps: [HttpClient],
-            },
-        }),
-    ],
-    bootstrap: [AppComponent],
+  declarations: [AppComponent, MapBoxComponent, SitePanelSheetComponent],
+  entryComponents: [SitePanelSheetComponent], // inside we put the modalComponent
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    RecordModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
