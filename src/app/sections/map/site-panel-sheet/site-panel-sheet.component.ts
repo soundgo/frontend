4583741256e7f1e89c4b9c236ba263f3c92b67f7
Sergio Clebal <sergioclebal@gmail.com>
2019-04-02 12:22:36 +0200

    fixed record audio in site when no authenticated

--- src/app/sections/map/site-panel-sheet/site-panel-sheet.component.ts
+++ src/app/sections/map/site-panel-sheet/site-panel-sheet.component.ts
@@ -1,6 +1,7 @@
 import {Component, Inject} from '@angular/core';
 import {MAT_BOTTOM_SHEET_DATA, MatBottomSheetRef} from '@angular/material';
 import {ApiService} from '../../../services/api.service';
+import {ContextService} from '../../../services/context.service';
 
 @Component({
     selector: 'app-site-panel-sheet',
@@ -9,9 +10,13 @@ import {ApiService} from '../../../services/api.service';
 })
 export class SitePanelSheetComponent {
 
+    canRecord = false;
+
     constructor(private api: ApiService,
+                private context: ContextService,
                 private bottomSheetRef: MatBottomSheetRef<SitePanelSheetComponent>,
                 @Inject(MAT_BOTTOM_SHEET_DATA) public data: any) {
+        this.canRecord = this.context.getAuth().getValue() !== null;
     }
 
     closeSitePanel() {
