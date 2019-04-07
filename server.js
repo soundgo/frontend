04112a19cd77d61d0947918de51ce83f5f1ba7c1
Sergio Clebal <sergioclebal@gmail.com>
2019-04-08 00:09:14 +0200

    Fix HTTPs redirect

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const sslRedirect = require('heroku-ssl-redirect');
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/frontend'));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use((req, res, next) => {
-        if (req.header('x-forwarded-proto') !== 'https') {
-            res.redirect(`https://${req.header('host')}${req.url}`);
-        } else {
-            next();
-        }
-    });
-}
+// enable ssl redirect
+app.use(sslRedirect());
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, '/dist/frontend/index.html'));
