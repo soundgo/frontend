d068bb84ba7c6af4cf6d89c8698b98d57fda5e45
Sergio Clebal <sergioclebal@gmail.com>
2019-04-10 21:48:37 +0200

    Some changes in edit profile

--- src/app/sections/account/components/profile/profile.component.ts
+++ src/app/sections/account/components/profile/profile.component.ts
@@ -1,46 +1,45 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { User } from 'src/app/shared/models/User';
-import { ContextService } from 'src/app/services/context.service';
-import { MatDialogRef, MatDialog, MAT_BOTTOM_SHEET_DATA } from '@angular/material';
-import { EditProfileComponent } from 'src/app/sections/account/components/edit-profile/edit-profile.component';
+import {Component, OnInit, Inject} from '@angular/core';
+import {User} from 'src/app/shared/models/User';
+import {ContextService} from 'src/app/services/context.service';
+import {MatDialogRef, MatDialog, MAT_BOTTOM_SHEET_DATA} from '@angular/material';
+import {EditProfileComponent} from 'src/app/sections/account/components/edit-profile/edit-profile.component';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+    selector: 'app-profile',
+    templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
 
-  user: User;
-  isAdvertiser: boolean;
+    user: User;
+    isAdvertiser: boolean;
 
-  constructor(private context: ContextService,
-    public dialogRef: MatDialogRef<ProfileComponent>, protected dialog: MatDialog,) {
-    this.user = this.context.getUser().getValue();
-    this.isAdvertiser = this.context.getAuth().getValue() === 'advertiser' ? true : false;
-    console.log(this.user)
-    console.log(this.isAdvertiser)
-    
-  }
+    constructor(private context: ContextService,
+                public dialogRef: MatDialogRef<ProfileComponent>,
+                protected dialog: MatDialog) {
+        this.user = this.context.getUser().getValue();
+        this.isAdvertiser = this.context.getAuth().getValue() === 'advertiser';
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  onClose() {
-    this.dialogRef.close();
-  }
+    onClose() {
+        this.dialogRef.close();
+    }
 
-  editProfile() {
-    this.dialog.open(EditProfileComponent, {
-        width: '320px',
-        data: {
-            user: this.context.getUser().getValue(),
-        }
-    }).afterClosed().subscribe(result => {
-      if (result) {
-          this.user = result;
-      }
-  });
-}
+    editProfile() {
+        this.dialog.open(EditProfileComponent, {
+            width: '320px',
+            data: {
+                user: this.user,
+            }
+        }).afterClosed().subscribe(result => {
+            if (result) {
+                this.user = result;
+                this.context.setUser(this.user);
+            }
+        });
+    }
 
 }
