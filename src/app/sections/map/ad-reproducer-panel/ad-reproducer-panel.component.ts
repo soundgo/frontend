c3d6edb722bd85f9f3ab2d915bba6e718016ee73
Sergio Clebal <sergioclebal@gmail.com>
2019-04-12 20:58:23 +0200

    WIP

--- src/app/sections/map/ad-reproducer-panel/ad-reproducer-panel.component.ts
+++ src/app/sections/map/ad-reproducer-panel/ad-reproducer-panel.component.ts
@@ -37,7 +37,7 @@ export class AdReproducerPanelComponent implements OnInit, OnDestroy {
     onFinish(params) {
         this.api.adReproduced(this.ad.id).then((response: any) => {
             const user = this.context.getUser().getValue();
-            if (!response) {
+            if (!response.listened) {
                 const {timeToListenAnAdvertisement} = this.context.getConfig().getValue();
                 const duration = params.duration * timeToListenAnAdvertisement;
                 user.minutes += duration;
