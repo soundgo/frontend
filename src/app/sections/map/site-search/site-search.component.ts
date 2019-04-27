7a9ea9c95386dab87871aa200da717e5649ec226
Sergio Clebal <sergioclebal@gmail.com>
2019-04-27 19:25:55 +0200

    WIP

--- src/app/sections/map/site-search/site-search.component.ts
+++ src/app/sections/map/site-search/site-search.component.ts
@@ -1,15 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
+import {ApiService} from '../../../services/api.service';
+import {ContextService} from '../../../services/context.service';
+import {MatBottomSheetRef} from '@angular/material';
+import {Site} from '../../../shared/models/Site';
 
 @Component({
-  selector: 'app-site-search',
-  templateUrl: './site-search.component.html',
-  styleUrls: ['./site-search.component.scss']
+    selector: 'app-site-search',
+    templateUrl: './site-search.component.html',
+    styleUrls: ['./site-search.component.scss']
 })
-export class SiteSearchComponent implements OnInit {
+export class SiteSearchComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    private isLoading = false;
+    private sites: any[] = [{
+        id: 1,
+        latitude: 34.5,
+        longitude: 34.5,
+        name: 'Site example 1',
+        description: 'Site description 1'
+    }];
+    private sitesFound: any[] = [];
+    private term: string;
 
-  ngOnInit() {
-  }
+    constructor(
+        private api: ApiService,
+        private context: ContextService,
+        private bottomSheetRef: MatBottomSheetRef<SiteSearchComponent>,
+        private cdr: ChangeDetectorRef
+    ) {
+    }
+
+    ngOnInit() {
+        /*this.isLoading = true;
+        this.api.getSites().then((response: Site[]) => {
+            this.sites = response;
+            this.isLoading = false;
+            if (!this.cdr['destroyed']) {
+                this.cdr.detectChanges();
+            }
+        });*/
+    }
+
+    ngOnDestroy() {
+        this.cdr.detach();
+    }
+
+    searchSites($event) {
+        this.term = $event.target ? $event.target.value : this.term;
+        this.sitesFound = this.sites.filter((site: any) => {
+            return site.name.toLowerCase().includes(this.term.toLowerCase()) ||
+                site.description.toLowerCase().includes(this.term.toLowerCase());
+        }).slice(0, 10);
+    }
+
+
+    onClose() {
+        this.bottomSheetRef.dismiss();
+    }
+
+    flyTo(idx) {
+        const {latitude, longitude} = this.sitesFound[idx];
+        this.context.getMap().getValue().flyTo({
+            center: [longitude, latitude]
+        });
+    }
+
+    isSiteFoundAvailable() {
+        return this.sitesFound && this.sitesFound.length !== 0 && !this.isLoading;
+    }
 
 }
